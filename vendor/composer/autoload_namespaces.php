aad47874a22ffbe2cef5ac57ad1f777f88350ae2
Maurice Renck <maurice@maurice-renck.de>
2020-11-12 12:26:14 +0100

    chore: Monolithic Plugin Setup

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'claviska' => array($vendorDir . '/claviska/simpleimage/src'),
+    'Michelf' => array($vendorDir . '/michelf/php-smartypants'),
     'IndieWeb' => array($vendorDir . '/indieweb/mention-client/src'),
 );
