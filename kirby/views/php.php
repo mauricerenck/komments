3cbcc7be21c41cc16a8a6f315230655d755bf920
Maurice Renck <maurice@maurice-renck.de>
2020-11-12 13:42:50 +0100

    chore: add kirby folder for autoload

---
+++ kirby/views/php.php
@@ -0,0 +1,11 @@
+<?php include __DIR__ . '/snippets/header.php' ?>
+
+  <p class="notice">
+    This page is currently offline. We are very sorry for the inconvenience and will fix it as soon as possible.
+  </p>
+  <p class="admin-advice">
+    Advice for developers and administrators:<br>
+    Change the PHP version to 7.2, 7.3 or 7.4 (PHP 7.3 or 7.4 are recommended)
+  </p>
+
+<?php include __DIR__ . '/snippets/footer.php' ?>
