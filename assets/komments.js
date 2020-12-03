6e5e2553d634a2cbcc0e0a5b0c4d3d0cda5d70d7
Maurice Renck <maurice@maurice-renck.de>
2020-12-03 12:21:15 +0100

    improvement: remove onload, because this may cause the plugin not to init

--- assets/komments.js
+++ assets/komments.js
@@ -1,112 +1,110 @@
-window.onload = function () {
-    const komment = document.querySelector('#kommentform #komment')
-    const kommentForm = document.querySelector('#kommentform')
-    const kommentEmail = document.querySelector('#kommentform #email')
-    const kommentAuthor = document.querySelector('#kommentform #author')
-    const kommentUrl = document.querySelector('#kommentform #author_url')
-    const timeField = document.querySelector('#kommentform .cts')
-    const submitField = document.querySelector('#kommentform input[type=submit]')
-    const spamIndicatorProgress = document.querySelector('#kommentform .spam-indicator .progress')
-    const sendingIndicator = document.querySelector('#kommentform .loader')
-    const formFeedback = document.querySelector('#kommentform .form-feedback')
+const komment = document.querySelector('#kommentform #komment')
+const kommentForm = document.querySelector('#kommentform')
+const kommentEmail = document.querySelector('#kommentform #email')
+const kommentAuthor = document.querySelector('#kommentform #author')
+const kommentUrl = document.querySelector('#kommentform #author_url')
+const timeField = document.querySelector('#kommentform .cts')
+const submitField = document.querySelector('#kommentform input[type=submit]')
+const spamIndicatorProgress = document.querySelector('#kommentform .spam-indicator .progress')
+const sendingIndicator = document.querySelector('#kommentform .loader')
+const formFeedback = document.querySelector('#kommentform .form-feedback')
 
-    let duration = {
-        indicator: 0,
-        timer: null,
-        current: 0
+let duration = {
+    indicator: 0,
+    timer: null,
+    current: 0
+}
+
+const setIndicator = () => {
+
+    if (duration.current > 0) {
+        return
     }
 
-    const setIndicator = () => {
+    timeField.value = 0;
+    submitField.disabled = true;
 
-        if (duration.current > 0) {
-            return
+    duration.timer = window.setInterval(() => {
+        duration.indicator += 10;
+        spamIndicatorProgress.style.width = `${duration.indicator}%`
+
+        if (duration.indicator > 25) {
+            spamIndicatorProgress.classList.add('orange')
         }
 
-        timeField.value = 0;
-        submitField.disabled = true;
+        if (duration.indicator > 50) {
+            spamIndicatorProgress.classList.add('yellow')
+        }
 
-        duration.timer = window.setInterval(() => {
-            duration.indicator += 10;
-            spamIndicatorProgress.style.width = `${duration.indicator}%`
+        if (duration.indicator > 75) {
+            spamIndicatorProgress.classList.add('green')
+        }
 
-            if (duration.indicator > 25) {
-                spamIndicatorProgress.classList.add('orange')
-            }
+        if (duration.indicator >= 100) {
+            spamIndicatorProgress.classList.add('done')
+            submitField.disabled = false;
 
-            if (duration.indicator > 50) {
-                spamIndicatorProgress.classList.add('yellow')
-            }
+            window.clearInterval(duration.timer)
+        }
 
-            if (duration.indicator > 75) {
-                spamIndicatorProgress.classList.add('green')
-            }
+        duration.current++;
+        timeField.value = duration.current
+    }, 1000)
+}
 
-            if (duration.indicator >= 100) {
-                spamIndicatorProgress.classList.add('done')
-                submitField.disabled = false;
 
-                window.clearInterval(duration.timer)
-            }
-
-            duration.current++;
-            timeField.value = duration.current
-        }, 1000)
+const sendKomment = (event) => {
+    if (!kommentForm) {
+        return true
     }
+    event.preventDefault();
 
+    const kommentFormAction = kommentForm.action;
+    sendingIndicator.classList.remove('loading-invisible');
 
-    const sendKomment = (event) => {
-        if (!kommentForm) {
-            return true
-        }
-        event.preventDefault();
+    window.fetch(kommentFormAction, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json', 'X-Return-Type': 'json' },
+        body: JSON.stringify(Object.fromEntries(new FormData(event.target))),
+    })
+        .then(function (response) {
+            sendingIndicator.classList.add('loading-invisible');
+            if (!response.ok) {
+                throw response;
+            }
 
-        const kommentFormAction = kommentForm.action;
-        sendingIndicator.classList.remove('loading-invisible');
-
-        window.fetch(kommentFormAction, {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json', 'X-Return-Type': 'json' },
-            body: JSON.stringify(Object.fromEntries(new FormData(event.target))),
+            return response;
         })
-            .then(function (response) {
-                sendingIndicator.classList.add('loading-invisible');
-                if (!response.ok) {
-                    throw response;
-                }
-
-                return response;
+        .then((response) => {
+            response.json().then(message => {
+                formFeedback.classList.remove('error')
+                formFeedback.classList.add('moderation-note')
+                formFeedback.innerHTML = message.message
+                kommentForm.reset();
             })
-            .then((response) => {
-                response.json().then(message => {
-                    formFeedback.classList.remove('error')
-                    formFeedback.classList.add('moderation-note')
-                    formFeedback.innerHTML = message.message
-                    kommentForm.reset();
-                })
+        })
+        .catch((error) => {
+            error.json().then(error => {
+                formFeedback.classList.add('error')
+                formFeedback.classList.remove('moderation-note')
+                formFeedback.innerHTML = error.message
+
             })
-            .catch((error) => {
-                error.json().then(error => {
-                    formFeedback.classList.add('error')
-                    formFeedback.classList.remove('moderation-note')
-                    formFeedback.innerHTML = error.message
+        })
+}
 
-                })
-            })
-    }
+kommentEmail.style.display = 'none';
+kommentAuthor.style.display = 'none';
+kommentUrl.style.display = 'none';
 
-    kommentEmail.style.display = 'none';
-    kommentAuthor.style.display = 'none';
-    kommentUrl.style.display = 'none';
+komment.addEventListener('focus', () => {
+    setIndicator();
+    kommentEmail.style.display = 'block';
+    kommentAuthor.style.display = 'block';
+    kommentUrl.style.display = 'block';
+});
 
-    komment.addEventListener('focus', () => {
-        setIndicator();
-        kommentEmail.style.display = 'block';
-        kommentAuthor.style.display = 'block';
-        kommentUrl.style.display = 'block';
-    });
+kommentForm.addEventListener('submit', (event) => {
+    sendKomment(event);
+});
 
-    kommentForm.addEventListener('submit', (event) => {
-        sendKomment(event);
-    });
-
-};
