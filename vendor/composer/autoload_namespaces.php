316b5ba26ebfea37f686034891621678dc39b792
Maurice Renck <maurice@maurice-renck.de>
2021-02-05 13:51:25 +0100

    chore: remove kirby from deps

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'claviska' => array($vendorDir . '/claviska/simpleimage/src'),
-    'Michelf' => array($vendorDir . '/michelf/php-smartypants'),
     'IndieWeb' => array($vendorDir . '/indieweb/mention-client/src'),
 );
