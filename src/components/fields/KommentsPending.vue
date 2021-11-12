c2b56e783221dcf4f99bbd236b255b9c2c9a9728
Maurice Renck <maurice@maurice-renck.de>
2021-11-12 13:03:18 +0100

    fix: set pending comments label via field settings #32

--- src/components/fields/KommentsPending.vue
+++ src/components/fields/KommentsPending.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="kommentsPendingCounter">
-    <k-headline>Pending Comments</k-headline>
-    <div class="count">{{ queuedComments }}</div>
-  </div>
+    <div class="kommentsPendingCounter">
+        <k-headline>{{ label }}</k-headline>
+        <div class="count">{{ queuedComments }}</div>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    queuedComments: Number,
-  },
-};
+    props: {
+        queuedComments: Number,
+        label: String,
+    },
+}
 </script>
 
 <style>
 .kommentsPendingCounter {
-  position: relative;
-  padding: 1rem;
-  background: #fff;
-  box-shadow: var(--box-shadow-item);
-  text-align: center;
+    position: relative;
+    padding: 1rem;
+    background: #fff;
+    box-shadow: var(--box-shadow-item);
+    text-align: center;
 }
 
 .kommentsPendingCounter .count {
-  font-size: var(--text-5xl);
+    font-size: var(--text-5xl);
 }
 </style>
