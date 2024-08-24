cb1dcafc38273f022ec910406a23b4f3536a269f
Maurice Renck <maurice@maurice-renck.de>
2024-08-24 18:31:34 +0200

    feat: akismet spam detection

--- lib/KommentReceiver.php
+++ lib/KommentReceiver.php
@@ -4,12 +4,16 @@ namespace mauricerenck\Komments;
 
 use Kirby\Toolkit\V;
 use Kirby\Toolkit\Str;
+use Kirby\Http\Remote;
 
 class KommentReceiver {
 
-    public function __construct(private ?array $autoPublish = null, private ?bool $autoPublishVerified = null) {
+    public function __construct(private ?array $autoPublish = null, private ?bool $autoPublishVerified = null, private ?bool $akismet = null, private ?string $akismetApiKey = null, private ?bool $debug = null) {
         $this->autoPublish = $autoPublish ?? option('mauricerenck.komments.moderation.autoPublish', []);
         $this->autoPublishVerified = $autoPublishVerified ?? option('mauricerenck.komments.moderation.publish-verified', false);
+        $this->akismet = $akismet ?? option('mauricerenck.komments.spam.akismet', false);
+        $this->akismetApiKey = $akismetApiKey ?? option('mauricerenck.komments.spam.akismet_api_key', '');
+        $this->debug = $debug ?? option('mauricerenck.komments.debug', false);
     }
 
     public function validateFields(array $fields): array {
@@ -34,7 +38,7 @@ class KommentReceiver {
         return $inValidFields;
     }
 
-    public function getSpamlevel(array $fields): int
+    public function getSpamlevel(array $fields, $page): int
     {
         $spamlevel = 0;
         if (V::notEmpty($fields['url'])) {
@@ -60,7 +64,7 @@ class KommentReceiver {
             $spamlevel += 60;
         }
 
-        // TODO akismet check
+        $spamlevel += $this->akismetCheck($fields, $page);;
 
         return $spamlevel > 100 ? 100 : $spamlevel;
     }
@@ -112,5 +116,50 @@ class KommentReceiver {
     public function createSafeString(string $fieldValue): string
     {
         return Str::unhtml($fieldValue);
+    }
+
+    public function akismetCheck(array $fields, $page): int
+    {
+        if(!$this->akismet) {
+            return 0;
+        }
+
+        try {
+            $data = [
+                'api_key' => urlencode($this->akismetApiKey),
+                'blog' => urlencode(site()->url()),
+                'user_ip' => urlencode($_SERVER['REMOTE_ADDR']),
+                'user_agent' => urlencode($_SERVER['HTTP_USER_AGENT']),
+                'referrer' => urlencode($_SERVER['HTTP_REFERER']),
+                'permalink' => urlencode($page->permalink()),
+                'comment_type' => urlencode('comment'),
+                'comment_author' => urlencode($fields['author']),
+                'comment_author_email' => urlencode($fields['author_email'] ?? ''),
+                'comment_author_url' => urlencode($fields['author_url']),
+                'comment_content' => urlencode($fields['comment']),
+                'honeypot_field_name' => urlencode('url'),
+            ];
+
+            if($this->debug) {
+                $data['is_test'] = true;
+            }
+
+            $response = Remote::request('https://rest.akismet.com/1.1/comment-check', [
+                'method' => 'POST',
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                ],
+                'data' => $data,
+            ]);
+
+            if ($response->code() !== 200) {
+                return 0;
+            }
+
+            return $response->content() == 'true' ? 100 : 0;
+        } catch (\Exception $e) {
+            return 0;
+        }
+
     }
 }
