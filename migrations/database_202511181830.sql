01f948c55fea08e81723dcfc924f7e45784b4aad
Maurice Renck <maurice@maurice-renck.de>
2025-11-18 18:34:44 +0100

    fix: migration history

---
+++ migrations/database_202511181830.sql
@@ -0,0 +1,4 @@
+-- Always add the migration version to the migrations table:
+INSERT INTO migrations (version) VALUES (202511122120);
+INSERT INTO migrations (version) VALUES (202511171100);
+INSERT INTO migrations (version) VALUES (202511181830);
