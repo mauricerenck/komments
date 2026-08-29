5f92cf9f7c31000e88398d4319ff9e9d20d65155
Maurice Renck <maurice@maurice-renck.de>
2026-08-29 14:10:44 +0200

    chore: config

--- site/config/config.php
+++ site/config/config.php
@@ -4,61 +4,72 @@ return [
     'debug' => true,
     'languages' => false,
 
-    'email' => [
-        'transport' => [
-            'type' => 'smtp',
-            'host' => 'localhost',
-            'port' => 1025,
-            'security' => false
-        ]
-    ],
-    'mauricerenck' => [
-        'komments' => [
-            'debug' => true,
-            'migrations' => [
-                'comments' => false,
-            ],
-            'webmentions.sendReplies' => true,
-            'auto-publish-verified' => true,
-            'auto-delete-spam' => false,
-            'autoDisable.ttl' => 0,
-            'privacy' => [
-                'storeEmail' => false,
-            ],
-            'notifications' => [
-                'skipSpam' => false,
-                'email' => [
-                    'enable' => true,
-                    'sender' => 'test@domain.tld',
-                    'emailReceiverList' => ['test@domain.tld'],
-                ]
-            ],
-            'moderation' => [
-                'autoPublish' => [
-                    'test@phpunit.de'
-                ]
-            ],
-            'storage' => [
-                // 'type' => 'markdown',
-                'type' => 'sqlite',
-                'sqlitePath' => '.sqlite',
-            ],
-            'spam' => [
-                'delete' => false,
-                'verification' => [
-                    'enabled' => true,
-                    'filterUnverified' => true,
-                    'ttl' => 1,
-                    'secret' => 'my-extremly-secure-secret',
-                    'autoPublish' => false,
-                    'deletionMode' => 'delete'
-                ],
-            ],
-            'avatar' => [
-                'service' => 'initials',
-                'size' => 64,
-            ]
-
+    'mauricerenck.komments' => [
+        'privacy' => [
+            'requireEmail' => false,
+            'requireAuthor' => false,
         ]
     ]
 ];
+// return [
+//     'debug' => true,
+//     'languages' => false,
+
+//     'email' => [
+//         'transport' => [
+//             'type' => 'smtp',
+//             'host' => 'localhost',
+//             'port' => 1025,
+//             'security' => false
+//         ]
+//     ],
+//     'mauricerenck.komments' => [
+//         'debug' => true,
+//         'migrations' => [
+//             'comments' => false,
+//         ],
+//         'webmentions.sendReplies' => true,
+//         'auto-publish-verified' => true,
+//         'auto-delete-spam' => false,
+//         'autoDisable.ttl' => 0,
+//         'privacy' => [
+//             'storeEmail' => false,
+//             'requireEmail' => false,
+//         ],
+//         'notifications' => [
+//             'skipSpam' => false,
+//             'email' => [
+//                 'enable' => true,
+//                 'sender' => 'test@domain.tld',
+//                 'emailReceiverList' => ['test@domain.tld'],
+//             ]
+//         ],
+//         'moderation' => [
+//             'autoPublish' => [
+//                 'test@phpunit.de'
+//             ]
+//         ],
+//         'storage' => [
+//             // 'type' => 'markdown',
+//             'type' => 'sqlite',
+//             'sqlitePath' => '.sqlite',
+//         ],
+//         'spam' => [
+//             'delete' => false,
+//             'verification' => [
+//                 'enabled' => false,
+//                 'filterUnverified' => true,
+//                 'ttl' => 1,
+//                 'secret' => 'my-extremly-secure-secret',
+//                 'autoPublish' => false,
+//                 'deletionMode' => 'delete'
+//             ],
+//         ],
+//         'avatar' => [
+//             'service' => 'initials',
+//             'size' => 64,
+//         ]
+
+//     ]
+
+// ];
